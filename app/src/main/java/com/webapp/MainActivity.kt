045b8c3bb21be766de3dc0ae86650000f45b794c
txret <txret@pm.me>
2024-02-05 13:56:15 +0100

    Credential storage

--- app/src/main/java/com/webapp/MainActivity.kt
+++ app/src/main/java/com/webapp/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.webapp
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.webkit.CookieManager
-import android.webkit.GeolocationPermissions
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 
 import android.annotation.SuppressLint
 
@@ -20,23 +18,31 @@ class MainActivity : ComponentActivity() {
     private lateinit var webView: WebView
     private var locationRequestOrigin: String? = null
     private var locationRequestCallback: GeolocationPermissions.Callback? = null
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        webView = findViewById(R.id.webview)
-        @SuppressLint("SetJavaScriptEnabled")
-        webView.settings.javaScriptEnabled = true
-        webView.settings.allowContentAccess = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.useWideViewPort = true
-        webView.settings.setGeolocationEnabled(true)
-        webView.settings.userAgentString =
-            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36"
+        sharedPreferences = initEncryptedSharedPreferences()
+        webView = setupWebView()
+        webView.loadUrl(getString(R.string.app_url))
+    }
 
-        // keep all clicked URLs in the webview
-        webView.webViewClient = object : WebViewClient() {
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView(): WebView {
+        val wv: WebView = findViewById(R.id.webview)
+        wv.settings.apply {
+            javaScriptEnabled = true
+            allowContentAccess = true
+            domStorageEnabled = true
+            useWideViewPort = true
+            setGeolocationEnabled(true)
+            userAgentString = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Mobile Safari/537.36"
+        }
+
+        wv.webViewClient = object : WebViewClient() {
+            // keep new URLs in webview
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
                 request: WebResourceRequest?
@@ -46,41 +52,57 @@ class MainActivity : ComponentActivity() {
                 }
                 return true
             }
+
+            // inject credential management
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                credentialsAutofill()
+            }
         }
 
-        // required for location permission request
-        webView.webChromeClient = object : WebChromeClient() {
+        wv.webChromeClient = object : WebChromeClient() {
             override fun onGeolocationPermissionsShowPrompt(
                 origin: String?,
                 callback: GeolocationPermissions.Callback?
             ) {
                 super.onGeolocationPermissionsShowPrompt(origin, callback)
-                locationRequestOrigin = null
-                locationRequestCallback = null
-                if (ContextCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    locationRequestOrigin = origin
-                    locationRequestCallback = callback
-                    requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                } else {
-                    callback?.invoke(origin, true, false)
-                }
+                locationPermissions(origin, callback)
             }
         }
 
-        // enable cookies
+        wv.addJavascriptInterface(object : Any() {
+            @JavascriptInterface
+            fun saveUserName(username: String) {
+                sharedPreferences.edit().putString("username", username).apply()
+            }
+            @JavascriptInterface
+            fun savePassword(password: String) {
+                sharedPreferences.edit().putString("password", password).apply()
+            }
+        }, "Android")
+
+
         val cookieManager = CookieManager.getInstance()
         cookieManager.setAcceptCookie(true)
-        cookieManager.setAcceptThirdPartyCookies(webView, true)
+        cookieManager.setAcceptThirdPartyCookies(wv, true)
 
-        // load URL
-        webView.loadUrl(getString(R.string.app_url))
+        return wv
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // location permissions
+    private fun locationPermissions(origin: String?, callback: GeolocationPermissions.Callback?) {
+        locationRequestOrigin = null
+        locationRequestCallback = null
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationRequestOrigin = origin
+            locationRequestCallback = callback
+            requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            callback?.invoke(origin, true, false)
+        }
     }
 
-    // request location permission
     private val requestLocationPermissionLauncher: ActivityResultLauncher<String> =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
@@ -89,4 +111,45 @@ class MainActivity : ComponentActivity() {
                 locationRequestCallback?.invoke(locationRequestOrigin, false, false)
             }
         }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // credentials management
+    private fun initEncryptedSharedPreferences(): SharedPreferences {
+        // Create or retrieve the MasterKey for encryption/decryption
+        val masterKey = MasterKey.Builder(applicationContext)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        // Initialize EncryptedSharedPreferences
+        return EncryptedSharedPreferences.create(
+            applicationContext,
+            "encrypted_prefs",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    // called on each load
+    private fun credentialsAutofill() {
+        val user = sharedPreferences.getString("username", "")
+        val pass = sharedPreferences.getString("password", "")
+
+        // Listen for form submission and update credentials if changed
+        val js = """
+            const user_fld = document.querySelector("input[name='username'],input[id='username'],input[autofill='username'],input[name='user'],input[id='user'],input[autofill='user']")
+            const pass_fld = document.querySelector("input[name*='password'],input[id*='password'],input[autofill*='password']")
+            document.querySelector("form").addEventListener("submit", function() {
+                if (user_fld) Android.saveUserName(user_fld.value)
+                if (pass_fld) Android.savePassword(pass_fld.value)
+            })
+            
+            if (user_fld && '$user') user_fld.value = '$user'
+            if (pass_fld && '$pass') pass_fld.value = '$pass'
+        """.trimIndent()
+
+        webView.post {
+            webView.evaluateJavascript(js, null)
+        }
+    }
 }
